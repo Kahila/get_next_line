2431d57f6bcde0776ad022f35a3733721f4e5fe4
Ado KALOMBO <akalombo@c4r11s7.wethinkcode.co.za>
2019-07-13 12:22:49 +0200

    libft

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: akalombo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 09:41:32 by akalombo          #+#    #+#             */
-/*   Updated: 2019/07/11 03:50:45 by akalombo         ###   ########.fr       */
+/*   Updated: 2019/07/13 12:21:20 by akalombo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char		*ft_strnew(size_t size)
 	char *str;
 
 	str = ft_memalloc(size + 1);
-    ft_bzero(str, size + 1);
+	ft_bzero(str, size + 1);
 	if (!str)
 		return (NULL);
 	str[ft_strlen(str) + 1] = '\0';
